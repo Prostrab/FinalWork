cd63b69f4d6a91d7ba24d4a7b57b8538a71c8e09
Prostrab <prostrab@gmail.com>
2023-04-02 02:53:18 +0300

    Дописал программу, закончив проект

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -1,1 +1,15 @@
 ﻿string [] Array = {"а" ,"ввф" ,"ddd2" , "a102" , "ad", ".акаы", "ввв"};
+Console.Write("[");
+foreach (string Value in Array)
+{
+if (Value.Length<=3)
+{
+    Console.Write($"{Value}" );
+    if (Value != Array[Array.Length-1])
+    Console.Write(",");
+    if (Value == Array[Array.Length-1])
+    {
+        Console.Write("]");
+    }
+}
+}
